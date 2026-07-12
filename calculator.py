35f3bfb2d07921037482d342bd56d57faf27f4d0
nabijon172-beep <nabijon172@gmail.com>
2026-07-13 00:45:47 +0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,23 +1,30 @@
-# Oddiy kalkulyator dasturi
-
 print("Kalkulyatorga xush kelibsiz!")
 
-son1 = float(input("Birinchi sonni kiriting: "))
-amal = input("Amalni tanlang (+, -, *, /): ")
-son2 = float(input("Ikkinchi sonni kiriting: "))
+davom_etish = True
 
-if amal == "+":
-    natija = son1 + son2
-elif amal == "-":
-    natija = son1 - son2
-elif amal == "*":
-    natija = son1 * son2
-elif amal == "/":
-    if son2 != 0:
-        natija = son1 / son2
+while davom_etish:
+    son1 = float(input("Birinchi sonni kiriting: "))
+    amal = input("Amalni tanlang (+, -, *, /): ")
+    son2 = float(input("Ikkinchi sonni kiriting: "))
+
+    if amal == "+":
+        natija = son1 + son2
+    elif amal == "-":
+        natija = son1 - son2
+    elif amal == "*":
+        natija = son1 * son2
+    elif amal == "/":
+        if son2 != 0:
+            natija = son1 / son2
+        else:
+            natija = "Nolga bo'lish mumkin emas!"
     else:
-        natija = "Nolga bo'lish mumkin emas!"
-else:
-    natija = "Noto'g'ri amal tanlandi"
+        natija = "Noto'g'ri amal tanlandi"
 
-print("Natija:", natija)
+    print("Natija:", natija)
+
+    javob = input("Yana hisoblashni xohlaysizmi? (ha/yo'q): ")
+    if javob != "ha":
+        davom_etish = False
+
+print("Kalkulyatordan foydalanganingiz uchun rahmat!")
